c299d6da2093aea051e1a70834743f9b8865099d
ww <415578597@qq.com>
2021-08-21 20:16:02 +0800

    Flex-box tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,18 @@ import HomePage from './pages/HomePage/HomePage';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
 
 import TestImport from './workflow/import_example/Test';
+import Flex from './workflow/flexbox/Flex';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/scss" exact component={TestImport} />
-
         <Route path="/" exact component={HomePage} />
-        <Route path="/order" exact component={HomePage} />
+        {/* <Route path="/order" exact component={OrderPage} /> */}
         {/* <ProtectedRoute path="/order" exact component={Order} /> */}
         {/* <Redirect exact from="/admin" to="/admin/dashboard" /> */}
+        <Route path="/scss" exact component={TestImport} />
+        <Route path="/flex" exact component={Flex} />
         <Route component={ErrorPage} />
       </Switch>
     </BrowserRouter>
