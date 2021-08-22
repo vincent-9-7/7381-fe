5cb67b2a32f4a6da0c8e9e1fe737fb97cacd5f24
ww <415578597@qq.com>
2021-08-22 20:53:54 +0800

    Set the button style

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import './HomePage.scss';
 
 function Home() {
-
   return (
     <div className="container">
       <div className="home-page">
-        <h1>Home Page</h1>
+        <h1>TOMATO</h1>
         <h2>Home Pages</h2>
+        <h3>www</h3>
+        <p>
+          Perfection fresh B-grade tomato form farmer Pick out your favourite ktjebandjandakjknalk
+          Fbjandkansdlknksdlnalkdnalkdnkandlkanslkd Kfas;ldma;sldosakdsmal kasmdlknalksdnlka
+        </p>
+        <button type="button" className="btn--see-more">See More</button>
       </div>
     </div>
   );
