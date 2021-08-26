5232a8004f2429fa49be6fd666b4e190ee2d8bda
ww <415578597@qq.com>
2021-08-27 00:37:35 +0800

    Footer

---
+++ src/components/PageFooter/PageFooter.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './PageFooter.scss';
+import B from '../../assets/img/B.jpg';
+import Market from '../../assets/img/market.jpg';
+
+function Footer() {
+  return (
+    <div className="page-footer">
+      <div className="container">
+        <div style={{display:"flex",justifyContent:"center",margin:"40px 0px"}}>
+          <div className="page-footer__logos">
+            <img src={B} alt="B" className="page-footer__logo--1" />
+            <img src={Market} alt="Market" className="page-footer__logo--2" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
