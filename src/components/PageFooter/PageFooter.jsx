2e6ef02fafcd13a1c27a36910401e2be3f2e2892
ww <415578597@qq.com>
2021-10-05 22:11:20 +0800

    Change page footer

--- src/components/PageFooter/PageFooter.jsx
+++ src/components/PageFooter/PageFooter.jsx
@@ -3,11 +3,22 @@ import './PageFooter.scss';
 import B from '../../assets/img/LogoB.svg';
 import Market from '../../assets/img/market.svg';
 
-function Footer() {
-  return (
-    <div className="page-footer">
+function Footer({ color }) {
+  return color === 'white' ? (
+    <div className="page-footer page-footer-white">
       <div className="container">
-        <div style={{display:"flex",justifyContent:"center",margin:"40px 0px"}}>
+        <div style={{ display: 'flex', justifyContent: 'center', padding: '20px 0px', marginTop:'10px' }}>
+          <div className="page-footer__logos">
+            <img src={B} alt="B" className="page-footer__logo--1" />
+            <img src={Market} alt="Market" className="page-footer__logo--2" />
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="page-footer page-footer-orange">
+      <div className="container">
+        <div style={{ display: 'flex', justifyContent: 'center', padding: '20px 0px', marginTop:'10px' }}>
           <div className="page-footer__logos">
             <img src={B} alt="B" className="page-footer__logo--1" />
             <img src={Market} alt="Market" className="page-footer__logo--2" />
