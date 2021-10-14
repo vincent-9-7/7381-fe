ce72ddab07f9ec21cd521486e331b75939dc69c1
ww <415578597@qq.com>
2021-10-14 15:56:02 +0800

    Change some style

--- src/components/Star/Star.jsx
+++ src/components/Star/Star.jsx
@@ -9,7 +9,7 @@ function Star(props) {
           rating={rating}
           numberOfStars={totalRating}
           starRatedColor="#FEE440"
-          starDimension="30px"
+          starDimension="25px"
           starSpacing="2px"
           starEmptyColor="rgba(253,247,122,0.3)"
           name="rating"
