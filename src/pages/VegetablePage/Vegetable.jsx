25f50a5f2de81609fa7503e814a07aa498e7d39b
ww <415578597@qq.com>
2021-10-16 00:11:09 +0800

    Code style

--- src/pages/VegetablePage/Vegetable.jsx
+++ src/pages/VegetablePage/Vegetable.jsx
@@ -27,16 +27,15 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Pagination from 'rc-pagination';
-import "rc-pagination/assets/index.css";
+import 'rc-pagination/assets/index.css';
 import { Multiselect } from 'multiselect-react-dropdown';
-import "../../components/Overview/Pagination.scss";
+import '../../components/Overview/Pagination.scss';
 import { getAllVegetableRequest } from '../../store/actions';
 import Header from '../../components/PageHeader/PageHeader';
 import Footer from '../../components/PageFooter/PageFooter';
 import OverviewList from '../../components/Overview/Overview';
 import Loading from '../../components/Loading/Loading';
 import SearchBar from '../../components/SearchBar/SearchBar';
-
 
 function VegetablePage() {
   const dispatch = useDispatch();
@@ -47,26 +46,29 @@ function VegetablePage() {
   const vegetables = useSelector((state) => state.itemslist.itemDetails);
 
   const objectArray = [
-    { key: "Processed products", value: "Group 1" },
-    { key: "B-grade products", value: "Group 1" }];
+    { key: 'Processed products', value: 'Group 1' },
+    { key: 'B-grade products', value: 'Group 1' },
+  ];
 
   const objectArray1 = [
-    { key: "Free Delivery", value: "Group 1" },
-    { key: "On Sale", value: "Group 1" }];
+    { key: 'Free Delivery', value: 'Group 1' },
+    { key: 'On Sale', value: 'Group 1' },
+  ];
 
   const objectArray2 = [
-    { key: "Australia", value: "Group 1" },
-    { key: "Queensland", value: "Group 1" }];
+    { key: 'Australia', value: 'Group 1' },
+    { key: 'Queensland', value: 'Group 1' },
+  ];
 
   const [state, setState] = React.useState(1);
-  const [option, setOption] = React.useState([{ key: "All", value: "Group 1" }]);
+  const [option, setOption] = React.useState([{ key: 'All', value: 'Group 1' }]);
 
-  const onChange = page => {
+  const onChange = (page) => {
     console.log(page);
     setState(page);
   };
 
-  const onSelect = option => {
+  const onSelect = (option) => {
     console.log(option);
     setOption(option);
   };
@@ -78,20 +80,19 @@ function VegetablePage() {
 
   let result = {};
   if (filter === 'Processed products') {
-    result = conditionFilter.filter(value => value.condition === 'Processed');
+    result = conditionFilter.filter((value) => value.condition === 'Processed');
   } else if (filter === 'B-grade products') {
-    result = conditionFilter.filter(value => value.condition === 'B-grade');
+    result = conditionFilter.filter((value) => value.condition === 'B-grade');
   } else if (filter === 'Free Delivery') {
-    result = conditionFilter.filter(value => value.category === 'Fruit');
+    result = conditionFilter.filter((value) => value.category === 'Fruit');
   } else if (filter === 'On Sale') {
-    result = conditionFilter.filter(value => value.state === 'QLD');
+    result = conditionFilter.filter((value) => value.state === 'QLD');
   } else if (filter === 'Australia') {
-    result = conditionFilter.filter(value => value.category === 'Fruit');
+    result = conditionFilter.filter((value) => value.category === 'Fruit');
   } else if (filter === 'Queensland') {
-    result = conditionFilter.filter(value => value.state === 'QLD');
-  }
-  else {
-    result = conditionFilter.filter(value => value.category === 'Vegetable');
+    result = conditionFilter.filter((value) => value.state === 'QLD');
+  } else {
+    result = conditionFilter.filter((value) => value.category === 'Vegetable');
   }
   // console.log(result);
 
@@ -103,84 +104,88 @@ function VegetablePage() {
   // console.log(filteredNewItems);
 
   const totalItem = result.length;
-  const startProduct = (state * 9) - (9 - 1);
+  const startProduct = state * 9 - (9 - 1);
   const endProduct = Math.min(startProduct + 9 - 1, totalItem);
 
-  return (<><Header color="white" />
-    <SearchBar />
-    {loading && <Loading />}
-    {!loading && (
-      <div className="overview-page--main-functions container">
-        {/* Filter */}
-        <div className="overview-filter">
-          <>
-            <Multiselect
-              options={objectArray}
-              displayValue="key"
-              singleSelect
-              placeholder='Condition'
-              onSelect={onSelect}
-            />
-            <hr />
-            <Multiselect
-              options={objectArray1}
-              displayValue="key"
-              singleSelect
-              placeholder='Special offers'
-              showCheckbox='false'
-              onSelect={onSelect}
-            />
-            <hr />
-            <Multiselect
-              options={objectArray2}
-              displayValue="key"
-              singleSelect
-              placeholder='Location'
-              onSelect={onSelect}
-            />
-            <hr />
-            {/* <button onClick={onClick} type='button' className='button'>
+  return (
+    <>
+      <Header color="white" />
+      <SearchBar />
+      {loading && <Loading />}
+      {!loading && (
+        <div className="overview-page--main-functions container">
+          {/* Filter */}
+          <div className="overview-filter">
+            <>
+              <Multiselect
+                options={objectArray}
+                displayValue="key"
+                singleSelect
+                placeholder="Condition"
+                onSelect={onSelect}
+              />
+              <hr />
+              <Multiselect
+                options={objectArray1}
+                displayValue="key"
+                singleSelect
+                placeholder="Special offers"
+                showCheckbox="false"
+                onSelect={onSelect}
+              />
+              <hr />
+              <Multiselect
+                options={objectArray2}
+                displayValue="key"
+                singleSelect
+                placeholder="Location"
+                onSelect={onSelect}
+              />
+              <hr />
+              {/* <button onClick={onClick} type='button' className='button'>
                 Reset your option
               </button> */}
-          </>
-        </div>
-        {/* List */}
-        <div style={{ display: 'flex', flexDirection: 'column', minWidth: '100%' }}>
-          {/* Count part */}
-          <div style={{
-            display: "flex", flexDirection: "row", justifyContent: "flex-end"
-          }}>
-            {startProduct}-{endProduct} of
-            <div style={{ fontWeight: "bold" }}>&nbsp;{totalItem} products</div>
+            </>
           </div>
-          <div className='overview-lists' style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-            {filteredNewItems.map((item) => (
-              <div key={item._id} className="overview-list">
-                <OverviewList
-                  image={item.imageAddress[0]}
-                  title={`${item.title} ${item.size}kg`}
-                  delivery="free delivery"
-                  description={item.description}
-                  oldPrice={false}
-                  price={`AU $${item.price}`}
-                  id={`${item._id}`}
-                  quantity={item.quantity}
-                  pageType="vegetable" />
-              </div>
-            ))}
+          {/* List */}
+          <div style={{ display: 'flex', flexDirection: 'column', minWidth: '100%' }}>
+            {/* Count part */}
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'flex-end',
+              }}
+            >
+              {startProduct}-{endProduct} of
+              <div style={{ fontWeight: 'bold' }}>&nbsp;{totalItem} products</div>
+            </div>
+            <div className="overview-lists" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+              {filteredNewItems.map((item) => (
+                <div key={item._id} className="overview-list">
+                  <OverviewList
+                    image={item.imageAddress[0]}
+                    title={`${item.title} ${item.size}kg`}
+                    delivery="free delivery"
+                    description={item.description}
+                    oldPrice={false}
+                    price={`AU $${item.price}`}
+                    id={`${item._id}`}
+                    quantity={item.quantity}
+                    pageType="vegetable"
+                  />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    )}
-    {/* <Pagination /> */}
-    {/* <Pagi totalItem={totalItem} value={props.state} onChange={onChangePage} /> */}
-    <Pagination
-      onChange={onChange}
-      current={state}
-      total={totalItem}
-      pageSize={9}
-    />
-    <Footer /></>);
-};
+      )}
+      {/* <Pagination /> */}
+      {/* <Pagi totalItem={totalItem} value={props.state} onChange={onChangePage} /> */}
+      <Pagination onChange={onChange} current={state} total={totalItem} pageSize={9} />
+      <Footer />
+    </>
+  );
+}
 
 export default VegetablePage;
